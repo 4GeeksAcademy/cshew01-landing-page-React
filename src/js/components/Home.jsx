41f01efd9aeca323005ac5531f47f598e5bdecc8
Chris Shewfelt <chris@apromoplace.com>
2024-12-11 19:17:31 +0000

    final commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,13 +7,59 @@ import Footer from "./footer.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+const library = [
+	{
+		image: "https://picsum.photos/id/1/500/325",
+		cardTitle: "Coding Time!",
+		cardText: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+		bottonUrl: "https://www.amazon.com/",
+	},
+	{
+		image: "https://picsum.photos/id/18/500/325",
+		cardTitle: "Relax",
+		cardText: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+		bottonUrl: "https://www.amazon.com/",
+	},
+	{
+		image: "https://picsum.photos/id/20/500/325",
+		cardTitle: "Study Session",
+		cardText: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+		bottonUrl: "https://www.amazon.com/",
+	},
+	{
+		image: "https://picsum.photos/id/130/500/325",
+		cardTitle: "Fly Away",
+		cardText: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+		bottonUrl: "https://www.amazon.com/",
+	},
+	{
+		image: "https://picsum.photos/id/28/500/325",
+		cardTitle: "Let's Go Hiking",
+		cardText: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+		bottonUrl: "https://www.amazon.com/",
+	},
+	{
+		image: "https://picsum.photos/id/54/500/325",
+		cardTitle: "Blue Skies",
+		cardText: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+		bottonUrl: "https://www.amazon.com/",
+	},
+]
+
+//how to make each card the same height?
 //create your first component
 const Home = () => {
 	return (
 		<>
 			<Navbar />
 			<Jumbotron />
-			<Card />
+			<div className="container">
+            	<div className="row justify-content-center p-1">					
+						{library.map((cards) => (
+							<Card cards={cards} />
+						))}					
+				</div>
+			</div>
 			<Footer />
 		</>
 	);
--- src/js/components/card.jsx
+++ src/js/components/card.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({cards}) => {
     return(
-        <div className="container m-1">
-            <div className="row">
-                <div className="col-4">
-                    <div className="card">
-                        <img src="https://picsum.photos/id/1/500/325" className="card-img-top"  alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div> 
+        
+        <div className="col-4 mb-4">
+            <div className="card h-100 d-flex flex-column">
+                <img src={cards.image} className="card-img-top"  alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{cards.cardTitle}</h5>
+                    <p className="card-text">{cards.cardText}</p>
+                </div>
+                <div class="card-footer mx-auto w-100 text-center">
+                   <a href={cards.buttonUrl} className="btn btn-primary align-self-end btn-block">Go somewhere</a>
+                </div>
             </div>
         </div>
     )  
